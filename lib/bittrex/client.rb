875edf53b0d1f17479e648bee1e832b41ca3ae20
Wojciech Rzepecki <wrzepecki@fessio.pl>
2017-09-03 17:02:11 +0200

    Commit

--- lib/bittrex/client.rb
+++ lib/bittrex/client.rb
@@ -17,7 +17,7 @@ module Bittrex
       nonce = Time.now.to_i
       query = URI.encode_www_form(params)
       response = connection.get do |req|
-        url = "#{HOST}/#{path}#{ params.empty? ? ('?apikey='+key+'&nonce='+nonce.to_s) : (params + '&apikey='+key+'&nonce='+nonce.to_s)}"
+        url = "#{HOST}/#{path}#{ query.empty? ? ('?apikey='+key+'&nonce='+nonce.to_s) : (query + '&apikey='+key+'&nonce='+nonce.to_s)}"
         puts url
         req.params.merge!(params)
         req.url(url)
